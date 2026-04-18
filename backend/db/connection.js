9c5a002ace581bf09e2d4a58920ae394bc7f012e
arlinchc <arlinchc@gmail.com>
2026-04-18 12:40:42 -0500

    Update: integración final backend, DB Render y ajustes front

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,23 +1,15 @@
 require("dotenv").config();
-
-
-console.log("USER:", process.env.DB_USER);
-console.log("PASS:", process.env.DB_PASSWORD);
-console.log("DB:", process.env.DB_NAME);
-console.log("HOST:", process.env.DB_HOST);
-console.log("PORT:", process.env.DB_PORT);
 const { Pool } = require("pg");
 
 let pool;
 
 if (process.env.DATABASE_URL) {
+  // Producción (Render)
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
   });
 } else {
+  // Desarrollo local
   pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -27,8 +19,7 @@ if (process.env.DATABASE_URL) {
   });
 }
 
-
-// debug conexión
+// Test de conexión
 pool.connect()
   .then(() => console.log("✅ Conectado a PostgreSQL"))
   .catch(err => console.error("❌ Error de conexión:", err));
